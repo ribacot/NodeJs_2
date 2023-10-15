fc4e7b0a594e8389386bed5706a8860d26ca3662
ribacot <remontdiskov1979@gmail.com>
2023-10-15 16:07:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 // ioSChbm7Sf1UR4VB
-// const mongoose = require("mongoose");
 const app = require("./app");
-// const DP_HOST =
-// 	"mongodb+srv://Andrii:ioSChbm7Sf1UR4VB@cluster0.etps8iu.mongodb.net/contacts_reader?retryWrites=true&w=majority";
-// mongoose.set("strictQuery", true);
-app.listen(5000, () => {
-	console.log("Server running. Use our API on port: 5000");
-	mongoose
-		// .connect(DP_HOST)
-		// .then(() => console.log("Database connect success"))
-		.catch((err) => console.log(err.messege));
-});
+const mongoose = require("mongoose");
+const DP_HOST =
+	"mongodb+srv://Andrii:ioSChbm7Sf1UR4VB@cluster0.etps8iu.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+
+mongoose
+	.connect(DP_HOST)
+	.then(() => {
+		console.log("Database connect success");
+		app.listen(5000, () => {
+			console.log("Server running. Use our API on port: 5000");
+		});
+	})
+	.catch((err) => {console.log(err.messege);
+	process.exit(1)});
