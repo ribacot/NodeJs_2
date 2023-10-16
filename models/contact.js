55af0371dd655dbf5fdc8c2364686073c9c0b3bd
ribacot <remontdiskov1979@gmail.com>
2023-10-17 00:37:27 +0300

    validate

--- models/contact.js
+++ models/contact.js
@@ -1,40 +1,40 @@
 const { Schema, model } = require("mongoose");
 
 const contactSchema = Schema(
-	{
-		name: {
-			type: String,
-			match: /^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-			required: true,
-		},
-		email: {
-			type: String,
-			match: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-			required: true,
-		},
-		phone: {
-			type: String,
-			match: /^[\(]\d{3}\[\)]\s\d{3}[\-]\d{4}$/,
-			required: true,
-		},
-		favorite: { type: Boolean, default: false },
-	},
-    // {
-    //     name: {
-    //       type: String,
-    //       required: [true, 'Set name for contact'],
-    //     },
-    //     email: {
-    //       type: String,
-    //     },
-    //     phone: {
-    //       type: String,
-    //     },
-    //     favorite: {
-    //       type: Boolean,
-    //       default: false,
-    //     },
-    //   },
+	// {
+	// 	name: {
+	// 		type: String,
+	// 		match: /^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+	// 		required: true,
+	// 	},
+	// 	email: {
+	// 		type: String,
+	// 		match: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+	// 		required: true,
+	// 	},
+	// 	phone: {
+	// 		type: String,
+	// 		match: /^[\(]\d{3}\[\)]\s\d{3}[\-]\d{4}$/,
+	// 		required: true,
+	// 	},
+	// 	favorite: { type: Boolean, default: false },
+	// },
+    {
+        name: {
+          type: String,
+          required: [true, 'Set name for contact'],
+        },
+        email: {
+          type: String,
+        },
+        phone: {
+          type: String,
+        },
+        favorite: {
+          type: Boolean,
+          default: false,
+        },
+      },
 	{ versionKey: false, timestamps: true }
 );
 
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,7 +4,7 @@ const addSchem = Joi.object({
 	name: Joi.string().required(),
 	email: Joi.string().required(),
 	phone: Joi.string()
-		.regex(/^[\()]\d{3}[\)]\s\d{3}[\-]\d{4}$/)
+		.regex(/^[\(]\d{3}[\)]\s\d{3}[\-]\d{4}$/)
 		.messages({ "string.pattern.base": `Phone number must have (123) 123-1234 type.` })
 		.required(),
 	favorite: Joi.boolean(),
