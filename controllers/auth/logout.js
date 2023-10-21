30e8ea68796d92b65a0ec9eb8320e23ff42351a3
ribacot <remontdiskov1979@gmail.com>
2023-10-21 17:46:43 +0300

    logout

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,9 +1,8 @@
-const { User } = require("../../models/user")
+const { User } = require("../../models/user");
 
-
-const logout=(req,res,next)=>{
-    req.user.token=null;
-    req.user.save();
-    res.status(200).json({message:'Logged out'});
-}
+const logout = async (req, res) => {
+	const { _id } = req.user;
+	await User.findByIdAndUpdate(_id, { token: "" });
+	res.status(200).json({ message: "No Content" });
+};
 module.exports = logout;
