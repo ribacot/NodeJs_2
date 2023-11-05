630a2acf1666922c225b973d48dc6ec2ed28fe50
ribacot <remontdiskov1979@gmail.com>
2023-11-05 18:05:01 +0200

    del_console.log

--- controllers/auth/resendVerifyEmail.js
+++ controllers/auth/resendVerifyEmail.js
@@ -4,13 +4,11 @@ const isUser = require("../../helpers/isUser");
 const resendVerifyEmail = async (req, res) => {
 	const { email } = req.body;
 	const user = await isUser({ searchValue: email, errorValue: "Email" });
-	console.log(user);
 	if (user.verify) {
 		throw HttpError(401, "Verification has already been passed");
 	}
 	await sendEmail(verificationMail(email, user.verificationCode));
 
-	sendEmail(verificationMail(email, user.verificationCode));
 
 	res.status(200).json({ mesage: "Verification email sent" });
 };
